ca0296d6f83cf755fd9aac6f11b377a7c039dbea
emerson_dalwampo <emerson.dalwampo@fireclay.ph>
2021-11-08 01:46:33 +0800

    Change to php

--- config.js
+++
@@ -1,9 +0,0 @@
-module.exports = config = {
-    mysqli: {
-        user: 'root',
-        host: 'localhost',
-        password: 'password',
-        database: 'web_mind_master',
-    },
-    port: '3001',
-}
