3531527ea4fc229f1664044df998dc2425f52393
sncrr <emersondalwampo1120@gmail.com>
2021-12-26 20:55:47 +0800

    Update send_verification.php

--- api/student/send_verification.php
+++ api/student/send_verification.php
@@ -30,10 +30,10 @@ try {
     //Content
     $mail->isHTML(true);                                  //Set email format to HTML
     $mail->Subject = 'Web Mind Verification Code';
-    $mail->Body    = 'Hi!</br>
-                    Someone tried to login to your Web Mind e-Learning Account.</br>
-                    If this was you, please use this following code to log in:</br>
-                    <h1>'.$code.'</h1></br>
+    $mail->Body    = 'Hi!<br>
+                    Someone tried to login to your Web Mind e-Learning Account.<br>
+                    If this was you, please use this following code to log in:<br>
+                    <h1>'.$code.'</h1><br>
                     If this wasn\'t you, please <b>reset your password</b> to secure your account.';
 
     $mail->AltBody = 'Hi! Someone tried to login to your Web Mind e-Learning Account. If this was you, please use this following code to log in:'.$code;
@@ -43,22 +43,5 @@ try {
 } catch (Exception $e) {
     echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
-
-// if($uuid && $password) {
-
-//     try {
-//         $sql = "UPDATE students SET ip='$ipAddress', browser='$browser' WHERE uuid='$uuid'";
-        
-//         $result = $db->query($sql);
-//     }
-//     catch (exception $e) {
-//         $result = false;
-//     }
-    
-//     echo json_encode($result);
-// }
-// else {
-//     echo json_encode(false);
-// }
 
 ?>
