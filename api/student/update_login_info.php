8ed2a85e63af2df5620ab3b45d9ab6be07342063
sncrr <emersondalwampo1120@gmail.com>
2021-12-26 20:56:28 +0800

    Update update_login_info.php

--- api/student/update_login_info.php
+++ api/student/update_login_info.php
@@ -3,67 +3,24 @@
 require_once('../config.php');
 require '../../vendor/autoload.php';
 
-// if(!isset($_POST['uuid'])) {
-//     echo json_encode(false);
-//     return;
-// }
-
-// $uuid = $_POST['uuid'];
-// $ipAddress = $_POST['ipAddress'];
-// $browser = $_POST['browser'];
-// $email = $_POST['email'];
-
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\Exception;
-
-//Create an instance; passing `true` enables exceptions
-$mail = new PHPMailer(true);
-$emailFrom = 'admin@webmind.com';
-$emailTo = 'emersondalwampo1120@gmail.com';
-
-try {
-    //Server settings
-    // $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-    // $mail->isSMTP();                                            //Send using SMTP
-    // $mail->Host       = 'smtp.example.com';                     //Set the SMTP server to send through
-    // $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    // $mail->Username   = 'user@example.com';                     //SMTP username
-    // $mail->Password   = 'secret';                               //SMTP password
-    // $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-    // $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
-
-    //Recipients
-    $mail->setFrom($emailFrom, 'Web Mind');
-    $mail->addAddress($emailTo);               //Name is optional
-
-    //Content
-    $mail->isHTML(true);                                  //Set email format to HTML
-    $mail->Subject = 'Here is the subject';
-    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
-    $mail->send();
-    echo 'Message has been sent.</br>From: '.$emailFrom.'</br>To:'.$emailTo;
-} catch (Exception $e) {
-    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+if(!isset($_POST['uuid'])) {
+    echo json_encode(false);
+    return;
 }
 
-// if($uuid && $password) {
+$uuid = $_POST['uuid'];
+$ipAddress = $_POST['ipAddress'];
+$browser = $_POST['browser'];
 
-//     try {
-//         $sql = "UPDATE students SET ip='$ipAddress', browser='$browser' WHERE uuid='$uuid'";
-        
-//         $result = $db->query($sql);
-//     }
-//     catch (exception $e) {
-//         $result = false;
-//     }
+try {
+    $sql = "UPDATE students SET ip='$ipAddress', browser='$browser' WHERE uuid='$uuid'";
     
-//     echo json_encode($result);
-// }
-// else {
-//     echo json_encode(false);
-// }
+    $result = $db->query($sql);
+}
+catch (exception $e) {
+    $result = false;
+}
+
+echo json_encode($result);
 
 ?>
