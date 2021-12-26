910d914e4a4c3e1c7278a4865cbafb3ebb28c077
sncrr <emersondalwampo1120@gmail.com>
2021-12-26 19:27:42 +0800

    Update update_login_info.php

--- api/student/update_login_info.php
+++ api/student/update_login_info.php
@@ -3,15 +3,15 @@
 require_once('../config.php');
 require '../../vendor/autoload.php';
 
-if(!isset($_POST['uuid'])) {
-    echo json_encode(false);
-    return;
-}
+// if(!isset($_POST['uuid'])) {
+//     echo json_encode(false);
+//     return;
+// }
 
-$uuid = $_POST['uuid'];
-$ipAddress = $_POST['ipAddress'];
-$browser = $_POST['browser'];
-$email = $_POST['email'];
+// $uuid = $_POST['uuid'];
+// $ipAddress = $_POST['ipAddress'];
+// $browser = $_POST['browser'];
+// $email = $_POST['email'];
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
@@ -22,26 +22,18 @@ $mail = new PHPMailer(true);
 
 try {
     //Server settings
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-    $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'smtp.example.com';                     //Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'user@example.com';                     //SMTP username
-    $mail->Password   = 'secret';                               //SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+    // $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    // $mail->isSMTP();                                            //Send using SMTP
+    // $mail->Host       = 'smtp.example.com';                     //Set the SMTP server to send through
+    // $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    // $mail->Username   = 'user@example.com';                     //SMTP username
+    // $mail->Password   = 'secret';                               //SMTP password
+    // $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    // $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
     //Recipients
-    $mail->setFrom('from@example.com', 'Mailer');
-    $mail->addAddress('joe@example.net', 'Joe User');     //Add a recipient
-    $mail->addAddress('ellen@example.com');               //Name is optional
-    $mail->addReplyTo('info@example.com', 'Information');
-    $mail->addCC('cc@example.com');
-    $mail->addBCC('bcc@example.com');
-
-    //Attachments
-    $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
-    $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+    $mail->setFrom('edalwampo20@gmail.com', 'Mailer');
+    $mail->addAddress('emersondalwampo1120@gmail.com.com');               //Name is optional
 
     //Content
     $mail->isHTML(true);                                  //Set email format to HTML
@@ -55,21 +47,21 @@ try {
     echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
 
-if($uuid && $password) {
+// if($uuid && $password) {
 
-    try {
-        $sql = "UPDATE students SET ip='$ipAddress', browser='$browser' WHERE uuid='$uuid'";
+//     try {
+//         $sql = "UPDATE students SET ip='$ipAddress', browser='$browser' WHERE uuid='$uuid'";
         
-        $result = $db->query($sql);
-    }
-    catch (exception $e) {
-        $result = false;
-    }
+//         $result = $db->query($sql);
+//     }
+//     catch (exception $e) {
+//         $result = false;
+//     }
     
-    echo json_encode($result);
-}
-else {
-    echo json_encode(false);
-}
+//     echo json_encode($result);
+// }
+// else {
+//     echo json_encode(false);
+// }
 
 ?>
